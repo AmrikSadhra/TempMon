f22abde28b34ae8d536a67b581e780d1ae4f6575
Amrik Sadhra <AmrikSadhra@gmail.com>
2017-11-04 18:40:08 +0000

    Generated React App, Added ChartJS and SerialPort

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import './App.css';
+import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { NavItem } from 'react-bootstrap';
+import ChartView from './components/ChartView';
+
+var FontAwesome = require('react-fontawesome');
+
+class App extends React.Component {
+ 
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <Navbar inverse={true} collapseOnSelect={true}>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <FontAwesome name="thermometer" size="2x" style={{ marginBottom: '5px' }} />
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav>
+                <NavItem eventKey={1} href="#">Home</NavItem>
+              </Nav>
+              <Nav pullRight={true}>
+                <NavItem eventKey={1} href="#"><FontAwesome name="cog" size="2x"/></NavItem>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <div className="Page-title">ArTM</div>
+        </div>
+        <div className="App-main">
+          <ChartView name="Banana" data={[0, 19, 3, 5, 2, 3]}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ChartView.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { Line } from 'react-chartjs-2';
+import * as Se
+
+export interface Props {
+  name: string;
+  data: number[];
+}
+
+function ChartView({ name, data }: Props) {
+  //   throw new Error('You could be a little more enthusiastic. :D');
+
+  var chartData = {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'Temperature (°C)',
+            data: [0,1,2,2,34,5],
+            backgroundColor: 'rgba(46, 204, 113,0.5)',
+            borderWidth: 1
+        }]
+      };
+  
+  let key = 'data';
+  chartData.datasets[key] = data;
+
+  return (
+    <div className="Chart">
+     <Line data={chartData}/>
+    </div>
+  );
+}
+
+export default ChartView;
