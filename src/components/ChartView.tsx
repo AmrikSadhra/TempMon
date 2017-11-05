a8f977525aaf6ff30a1006a1ddaec92c267faf4c
Amrik Sadhra <AmrikSadhra@gmail.com>
2017-11-05 22:42:19 +0000

    Added webpack dev server, CSS loader and config to get serialport package working

--- src/components/ChartView.tsx
+++ src/components/ChartView.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Line } from 'react-chartjs-2';
-import * as Se
+import * as SerialPort from 'serialport';
+
 
 export interface Props {
   name: string;
@@ -10,22 +11,24 @@ export interface Props {
 function ChartView({ name, data }: Props) {
   //   throw new Error('You could be a little more enthusiastic. :D');
 
+ 
+
   var chartData = {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: 'Temperature (°C)',
-            data: [0,1,2,2,34,5],
-            backgroundColor: 'rgba(46, 204, 113,0.5)',
-            borderWidth: 1
-        }]
-      };
-  
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: 'Temperature (°C)',
+      data: [0, 1, 2, 2, 34, 5],
+      backgroundColor: 'rgba(46, 204, 113,0.5)',
+      borderWidth: 1
+    }]
+  };
+
   let key = 'data';
   chartData.datasets[key] = data;
 
   return (
     <div className="Chart">
-     <Line data={chartData}/>
+      <Line data={chartData} />
     </div>
   );
 }
